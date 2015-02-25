94f044366ca7c672464a876749a41d279ad81680
James Smith <james@floppy.org.uk>
2015-02-25 20:44:14 +0000

    handle spaces in postcode

--- spec/find_address_spec.rb
+++ spec/find_address_spec.rb
@@ -26,4 +26,28 @@ describe "finding addresses" do
     EOF
   end
   
+  it "should extract address with spaces in postcodes" do
+    text = <<-EOF.gsub(/^ {6}/, '')
+      Dear Mr Smith,
+      
+      Down with this sort of thing
+      
+      Yours sincerely,
+      Bob Fish
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+    
+    expect(find_address text).to eq <<-EOF.gsub(/^ {6}/, '').strip
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+  end
+  
 end

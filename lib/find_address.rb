5698a36b617a5ec1530c29a6f8b757e6f97ff397
James Smith <james@floppy.org.uk>
2015-02-25 20:54:52 +0000

    extract more addresses

--- lib/find_address.rb
+++ lib/find_address.rb
@@ -1,6 +1,6 @@
 def find_address(text)
   # Terrible multi-line regexp
-  re = /y,\n.*?\n(.*[A-Z]{2}[0-9]{1,2} ?[0-9]{1}[A-Z]{2})\n/m
+  re = /[ys],\n+.*?\n(.*[A-Z]{2}[0-9]{1,2} ?[0-9]{1}[A-Z]{2})\n/m
   match = text.match re
   match ? match[1] : nil
 end
--- spec/find_address_spec.rb
+++ spec/find_address_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "finding addresses" do
   
-  it "should extract address only" do
+  it "should extract address without spaces in postcodes" do
     text = <<-EOF.gsub(/^ {6}/, '')
       Dear Mr Smith,
       
@@ -50,4 +50,56 @@ describe "finding addresses" do
     EOF
   end
   
+  it "should extract address with a gap before the address" do
+    text = <<-EOF.gsub(/^ {6}/, '')
+      Dear Mr Smith,
+      
+      Down with this sort of thing
+      
+      Yours sincerely,
+      
+      
+      Bob Fish
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+    
+    expect(find_address text).to eq <<-EOF.gsub(/^ {6}/, '').strip
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+  end
+
+  it "should extract address with a different signoff" do
+    text = <<-EOF.gsub(/^ {6}/, '')
+      Dear Mr Smith,
+      
+      Down with this sort of thing
+      
+      Best wishes,
+      
+      
+      Bob Fish
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+    
+    expect(find_address text).to eq <<-EOF.gsub(/^ {6}/, '').strip
+      6 Example Lane
+      Test Area
+      Horsham
+      W. Sussex
+      XX12 3XX
+    EOF
+  end
+
 end

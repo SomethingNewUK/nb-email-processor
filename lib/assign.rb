f0dd68a23bd41a976eb112233e45528ba4213740
James Smith <james@floppy.org.uk>
2015-03-11 21:27:36 +0000

    allow multiple text matches for point people

--- lib/assign.rb
+++ lib/assign.rb
@@ -5,13 +5,17 @@ end
 def assign(nb, text, id)
 
   person = nil
-  point_people.each_pair do |key, value|
-    if text.match value
-      person = key
+  name = nil
+  point_people.each_pair do |key, array|
+    array.each do |value|
+      if text.match value
+        person = key
+        name = value
+      end
     end
   end
   if person
-    puts "Reassigning user #{id} to #{person}"
+    puts "Reassigning user #{id} to #{name}"
     nb.call(:people, :update, id: id.to_i, person: { parent_id: person.to_i })
   end
 
